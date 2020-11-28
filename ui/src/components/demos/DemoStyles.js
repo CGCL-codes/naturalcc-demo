f14b169553a2f480e1a13342ea0496accb84cf9f
Yao Wan <wanyao1992@gmail.com>
2020-11-28 15:51:08 +0800

    init
    init

--- ui/src/components/demos/DemoStyles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { Tabs } from '@allenai/varnish';
-
-export const DemoVisualizationTabs = styled(Tabs)`
-  .ant-tabs-bar.ant-tabs-top-bar {
-    margin: 0;
-  }
-`;
